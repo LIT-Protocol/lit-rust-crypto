9548fce521473f289ea1366249b782355e96507d
Michael Lodder <mike@litprotocol.com>
2025-11-14 11:19:07 -0700

    update feature flags, add signing generators
    Signed-off-by: Michael Lodder <mike@litprotocol.com>

--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,81 @@ pub use elliptic_curve::hash2curve;
 
 #[cfg(feature = "arithmetic")]
 pub use elliptic_curve::ops as ec_ops;
+
+#[cfg(feature = "jubjub")]
+pub fn red_jubjub_signing_generator() -> jubjub::SubgroupPoint {
+    // use group::cofactor::CofactorGroup;
+    //
+    // const SPEND_AUTH_SIG_BASEPOINT_BYTES: [u8; 32] = [
+    //     48, 181, 242, 170, 173, 50, 86, 48, 188, 221, 219, 206, 77, 103, 101, 109, 5, 253, 28, 194,
+    //     208, 55, 187, 83, 117, 182, 233, 109, 158, 1, 161, 215,
+    // ];
+    // let pt: jubjub::ExtendedPoint = jubjub::AffinePoint::from_bytes(&SPEND_AUTH_SIG_BASEPOINT_BYTES)
+    //     .expect("to create extended point")
+    //     .into();
+    // pt.into_subgroup().expect("to convert to subgroup")
+    jubjub::SubgroupPoint::new_unchecked(
+        [
+            0x9fea675eb63e8cf6,
+            0x15ba8508eb7f13c5,
+            0x87a02da79c8b7ef8,
+            0xaf4897169c1851e,
+        ],
+        [
+            0xfb63146264e65a56,
+            0x77f3f8c6fd45d5e5,
+            0x8770a243986a6eb9,
+            0x6dde055ca112d037,
+        ],
+        [
+            0x00000001fffffffe,
+            0x5884b7fa00034802,
+            0x998c4fefecbc4ff5,
+            0x1824b159acc5056f,
+        ],
+        [
+            0x9fea675eb63e8cf6,
+            0x15ba8508eb7f13c5,
+            0x87a02da79c8b7ef8,
+            0x0af4897169c1851e,
+        ],
+        [
+            0xfb63146264e65a56,
+            0x77f3f8c6fd45d5e5,
+            0x8770a243986a6eb9,
+            0x6dde055ca112d037,
+        ],
+    )
+}
+
+#[cfg(feature = "pasta")]
+pub fn red_pallas_signing_generator() -> pallas::Point {
+    // use group::GroupEncoding;
+    // const SPEND_AUTH_SIG_BASEPOINT_BYTES: [u8; 32] = [
+    //     99, 201, 117, 184, 132, 114, 26, 141, 12, 161, 112, 123, 227, 12, 127, 12, 95, 68, 95, 62, 124,
+    //     24, 141, 59, 6, 214, 241, 40, 179, 35, 85, 183,
+    // ];
+    // pallas::Affine::from_bytes(&SPEND_AUTH_SIG_BASEPOINT_BYTES.into())
+    //     .expect("to affine point")
+    //     .into()
+    pallas::Point::new_unchecked(
+        [
+            0x490f248c0abc8698,
+            0x8f66e9e53afc5a4a,
+            0x8a4b3d0435fe217d,
+            0x1088a0c632c10d0b,
+        ],
+        [
+            0xe6900acd3de44a97,
+            0x24ecf6ed2f7b5e04,
+            0x79d25b841a7b961d,
+            0x0cfbbe1efe9c580b,
+        ],
+        [
+            0x34786d38fffffffd,
+            0x992c350be41914ad,
+            0xffffffffffffffff,
+            0x3fffffffffffffff,
+        ],
+    )
+}
